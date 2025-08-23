fc906cf66eda5a74634c94022a88885d5d768f62
Anshika Chauhan <chauhananshika533@gmail.com>
2025-08-23 16:21:23 +0530

    changes in view

--- my_app/views.py
+++ my_app/views.py
@@ -212,26 +212,24 @@ def load_locations(request):
     return JsonResponse(list(locations), safe=False)
 
 # new projects
-def newprojects(request, pk=None):
-    if pk:  # Detail page
-        project = newProject.objects.get(pk=pk)
-        if hasattr(project, 'brochure') and project.brochure and hasattr(project.brochure, 'url'):
-            project.fixed_brochure_url = project.brochure.url.replace('/image/upload/', '/raw/upload/')
-        elif project.brochure_url:
+from django.shortcuts import render
+from .models import newProject
+
+def newprojects(request):
+    # Get all projects ordered by newest first
+    projects = newProject.objects.all().order_by('-id')
+
+    # Set fixed_brochure_url for each project (Cloudinary compatible)
+    for project in projects:
+        if project.brochure:  # Cloudinary FileField
+            project.fixed_brochure_url = project.brochure.url  # direct Cloudinary URL
+        elif project.brochure_url:  # fallback if direct URL is stored
             project.fixed_brochure_url = project.brochure_url
         else:
             project.fixed_brochure_url = None
-        return render(request, 'detail.html', {'project': project})
-    else:  # List of projects
-        projects = newProject.objects.all().order_by('-id')
-        for project in projects:
-            if hasattr(project, 'brochure') and project.brochure and hasattr(project.brochure, 'url'):
-                project.fixed_brochure_url = project.brochure.url.replace('/image/upload/', '/raw/upload/')
-            elif project.brochure_url:
-                project.fixed_brochure_url = project.brochure_url
-            else:
-                project.fixed_brochure_url = None
-        return render(request, 'projects.html', {'projects': projects})
+
+    # Render projects.html template with projects context
+    return render(request, 'projects.html', {'projects': projects})
 
 
 #  Sell Form View
