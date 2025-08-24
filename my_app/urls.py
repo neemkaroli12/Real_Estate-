349ecb7468effe528334ad96a4acaee6a2726b46
root <root@srv963258.hstgr.cloud>
2025-08-24 18:06:04 +0000

    Save local changes before pulling

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,5 +1,4 @@
-# from django.contrib import admin
-# from django.urls import path
+ from django.contrib import admin from django.urls import path
 # from . import views
 # from django.conf.urls.static import static
 # from django.conf import settings
